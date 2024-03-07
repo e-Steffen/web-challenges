b121a052a1e045e6d9136a1a3c6e3f9f01570c42
Steffen Zierold <steffen.zierold@outlook.com>
2024-03-07 12:26:44 +0100

    2nd edit solution for task incl bonus

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -7,6 +7,10 @@ form.addEventListener("submit", (event) => {
   const formInputs = new FormData(event.target);
   const formData = Object.fromEntries(formInputs);
   console.log(formData);
-  event.target.reset();
+  // this is a solution with more code:
+  //   const ageValue = formData.age;
+  //   const badnessValue = formData.badness;
+  console.log(parseInt(formData.age, 10) + parseInt(formData.badness, 10));
   event.target.elements.firstName.focus();
+  event.target.reset();
 });
