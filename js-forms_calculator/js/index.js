2ca98246b9b0b4c064ba929512cc1cb31b69233d
Steffen Zierold <steffen.zierold@outlook.com>
2024-03-07 23:05:42 +0100

    first solution for task

---
+++ js-forms_calculator/js/index.js
@@ -0,0 +1,44 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+const resultOutput = document.querySelector('[data-js="result"]');
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let result;
+
+  // --v-- write your code here --v--
+  const formInputs = new FormData(event.target);
+  const singleData = Object.fromEntries(formInputs);
+  if (singleData.operator == "addition") {
+    result = add(
+      parseInt(singleData.numberA, 10),
+      parseInt(singleData.numberB, 10)
+    );
+  } else if (singleData.operator == "subtraction") {
+    result = subtract(singleData.numberA, singleData.numberB);
+  } else if (singleData.operator == "multiplication") {
+    result = multiply(singleData.numberA, singleData.numberB);
+  } else result = divide(singleData.numberA, singleData.numberB);
+
+  // --^-- write your code here --^--
+
+  resultOutput.textContent = result;
+});
