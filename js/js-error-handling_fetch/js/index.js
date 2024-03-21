0f503ebc1d8b5902435750a04745cff7645cbdfb
Steffen Zierold <steffen.zierold@outlook.com>
2024-03-21 10:35:24 +0100

    first solution for task

---
+++ js/js-error-handling_fetch/js/index.js
@@ -0,0 +1,27 @@
+console.clear();
+
+const userElement = document.querySelector(".user");
+const errorMesaage = document.querySelector(".error");
+
+async function getUser(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.data;
+}
+
+document.querySelectorAll("button[data-url]").forEach((button) =>
+  button.addEventListener("click", async (event) => {
+    try {
+      errorMesaage.innerText = "";
+      const user = await getUser(event.target.dataset.url);
+      userElement.innerHTML = `
+      <h2>${user.first_name} ${user.last_name}</h2>
+      <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
+      `;
+    } catch (error) {
+      errorMesaage.innerText =
+        "Soemthing doesn't working with the fetch, please inspect the URLs";
+      userElement.innerHTML = "";
+    }
+  })
+);
