04574fa6b32ccf278b1432393ae20ce167e7b08a
Steffen Zierold <steffen.zierold@outlook.com>
2024-03-21 10:14:35 +0100

    first solution for task

---
+++ js/js-error-handling_calculator/js/index.js
@@ -0,0 +1,33 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+const errorMessage = document.querySelector(".error");
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero!");
+    }
+    return a / b;
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+  try {
+    errorMessage.innerHTML = "";
+    const result = operations[operation](firstNumber, secondNumber);
+    output.innerText = result;
+  } catch (error) {
+    errorMessage.innerHTML =
+      "Please pass a number rather than 0 as divisor, thank you!";
+    output.innerText = "";
+  }
+});
