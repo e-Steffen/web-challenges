76348dd16bafb3a78ebe00683eaad346a38d1fe4
Steffen Zierold <steffen.zierold@outlook.com>
2024-04-05 11:41:09 +0200

    first solution task 2

---
+++ react/react-basics_hello-world-article/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorld />;
+}
+
+function HelloWorld() {
+  return (
+    <article>
+      <h1>Don not</h1>
+      <p>Please! Do not replace me with KI! I am human!</p>
+    </article>
+  );
+}
