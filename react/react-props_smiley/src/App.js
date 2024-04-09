54cf5b6e51f2864a7e157accd7f50e37023d9bb9
Steffen Zierold <steffen.zierold@outlook.com>
2024-04-09 11:36:16 +0200

    first solution for task

---
+++ react/react-props_smiley/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <h1>
+      <Smiley isHappy />
+    </h1>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return <>{isHappy ? "😁" : "😟"}</>;
+}
