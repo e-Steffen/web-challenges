43bfde47b85489ea7f706bd3c144eda9f613c157
Steffen Zierold <steffen.zierold@outlook.com>
2024-05-10 13:22:55 +0200

    first part of task

---
+++ react/react-global-state_lights/components/Light/index.js
@@ -0,0 +1,19 @@
+import { LightButton, Icon, Text, Name, State } from "./Light.styled";
+
+export default function Light({ id, name, onToggle, isOn }) {
+  // const [isOn, setIsOn] = useState(false);
+
+  // function handleToggle() {
+  //   setIsOn((isOn) => !isOn);
+  // }
+
+  return (
+    <LightButton type="button" onClick={() => onToggle(id)} $isOn={isOn}>
+      <Icon $isOn={isOn}>💡</Icon>
+      <Text>
+        <Name>{name}</Name>
+        <State>{isOn ? "On" : "Off"}</State>
+      </Text>
+    </LightButton>
+  );
+}
