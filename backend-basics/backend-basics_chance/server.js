5b2076157e32cb1002968ecbc6efd4ca60adc84a
Steffen Zierold <steffen.zierold@outlook.com>
2024-05-31 15:20:19 +0200

    edit solution for task

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -6,7 +6,7 @@ const chance = new Chance();
 export const server = createServer((request, response) => {
   const name = chance.name();
   const age = chance.age();
-  const profession = chance.profession({rank: true});
+  const profession = chance.profession({ rank: true });
   response.end(
     `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
   );
